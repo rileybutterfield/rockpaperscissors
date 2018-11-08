bd698256be1e972736e7d80cdf87a4a7b774e581
Riley Butterfield <riley@scripted.org>
2018-11-08 23:01:13 +0000

    day 1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,44 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice;
+var winner;
+var computerChoice;
+var choices = ['rock', "paper", "scissors"]
+
+$("#shoot").click(function(){
+   userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);
+    var compChoice = Math.floor((Math.random()) * 3)
+    var compChoiceText = choices[compChoice]
+      $("#computerChoice").text(compChoiceText);
+    console.log("computer has", compChoiceText)
+    var userChoiceWon
+    
+    if (userChoice === "rock"){
+        userChoiceWon = compChoiceText === "scissors"
+    }
+    else if (userChoice === "scissors"){
+        userChoiceWon = compChoiceText === "paper"
+    }
+    else if (userChoice === "paper"){
+        userChoiceWon = compChoiceText === "rock"
+    }
+    
+  
+       
+
+  console.log(userChoice, compChoiceText)
+    if (userChoiceWon === true){
+        $("#result").text("User wins!!!")
+    }
+    if (userChoiceWon === false){
+        $("#result").text("Computer wins!!!")
+    }
+    if (userChoice === compChoiceText){
+      $("#result").text("It's a tie!!!")
+    }
+});
 
 
 
